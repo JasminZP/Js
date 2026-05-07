1478f855df7ad894846134e27c3feec809e18fe4
Zinchuk Pavlo <150339743+JasminZP@users.noreply.github.com>
2026-05-07 10:17:00 +0300

    Add files via upload

---
+++ less1/script/js.js
@@ -0,0 +1,62 @@
+// console.log("Hello Pavlo");
+// console.warn("Hello Chubaka");
+// console.info("Hello Zinchuk");
+// console.error("Hello Andriyovich");
+//
+// var Markarasyan;
+// Markarasyan = 42;
+// console.log(Markarasyan);
+//
+// var Markarasuyan = "2";
+// console.log("Zminna: " + Markarasuyan);
+// console.log(Markarasuyan + Markarasyan);
+// console.log(Markarasyan - Markarasuyan);
+//
+//
+// var int = 10;
+// var float = 10.2;
+// var Bool = true;
+// var str = "str";
+// var nothing;
+// nothing = null;
+//
+
+// let num = 10;
+// console.log(num)
+//
+// const Pi = 3.14;
+
+//
+// let a = 21, b = 13480977814037843127890;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// let a, b;
+// a = 2 * 4;
+// b = 3 * (a = 3 - 10);
+// a = a + 1;
+// a += 1;
+//
+// a++;
+// a--;
+//
+// ++a;
+// --a;
+
+// let a = 2, b;
+// b = a++;
+// console.log(a);
+
+// let a = '2', b = '3';
+// console.log((a - 0) + (b - 0));
+// console.log(Number(a) + Number(b));
+
+// Interpolacia scheplena
+
+let name = "Pavlo";
+console.log("Achacha " + name + ", bigfat");
+let skill = "Web";
+console.log(`Skill: ${skill}`);
